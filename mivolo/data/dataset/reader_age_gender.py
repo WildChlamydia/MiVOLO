fix non zero ratio calculation

mivolo/data/dataset/reader_age_gender.py
@@ -462,10 +462,8 @@ def _cropout_asced_objs(
         crop[aobj_ymin:aobj_ymax, aobj_xmin:aobj_xmax] = crop_out_color
 
     # calc useful non-black area
-    remain_ratio = np.count_nonzero(crop) / (crop.shape[0] * crop.shape[1])
-    if (
-        crop.shape[0] < min_person_size or crop.shape[1] < min_person_size
-    ) or remain_ratio < min_person_aftercut_ratio:
+    remain_ratio = np.count_nonzero(crop) / (crop.shape[0] * crop.shape[1] * crop.shape[2])
+    if (crop.shape[0] < min_person_size or crop.shape[1] < min_person_size) or remain_ratio < min_person_aftercut_ratio:
         crop = None
         empty = True
 

mivolo/structures.py
@@ -317,7 +317,7 @@ class PersonAndFaceResult:
         person_ages = [r[0] for r in tracked_objects[pguid] if r[0] is not None] if pguid in tracked_objects else []
         person_genders = [r[1] for r in tracked_objects[pguid] if r[1] is not None] if pguid in tracked_objects else []
 
-        if not face_ages and not person_ages: # both empty
+        if not face_ages and not person_ages:  # both empty
             return None, None
 
         # You can play here with different aggregation strategies
@@ -434,7 +434,7 @@ class PersonAndFaceResult:
 
             obj_image[o_y1:o_y2, o_x1:o_x2] = 0
 
-        remain_ratio = np.count_nonzero(obj_image) / (obj_image.shape[0] * obj_image.shape[1])
+        remain_ratio = np.count_nonzero(obj_image) / (obj_image.shape[0] * obj_image.shape[1] * obj_image.shape[2])
         if remain_ratio < MIN_PERSON_CROP_AFTERCUT_RATIO:
             return None
 